7f42982fbadf9850c9e42e632665c628302a8fa1
skovhus <kenneth.skovhus@gmail.com>
2017-10-25 14:32:50 +0200

    Fix type definition (implicit any)

--- index.d.ts
+++ index.d.ts
@@ -4,21 +4,21 @@ export interface IAxiosRetryConfig {
   /**
    * The number of times to retry before failing
    * default: 3
-   * 
+   *
    * @type {number}
    */
   retries?: number,
   /**
    * A callback to further control if a request should be retried. By default, it retries if the result did not have a response.
    * default: error => !error.response
-   * 
+   *
    * @type {Function}
    */
   retryCondition?: (error: axios.AxiosError) => boolean
   /**
    * The delay between retries, it can be a simple number or function which recieves attempt count as argument ar returns delay as a nummber
    * default: 0
-   * 
+   *
    * @type {Function|Number}
    */
   delay?: (attempt: number) => number | number
@@ -28,7 +28,7 @@ export interface IAxiosRetry {
   (
     axios: axios.AxiosStatic,
     axiosRetryConfig?: IAxiosRetryConfig
-  )
+  ): void
 }
 
 declare const axiosRetry: IAxiosRetry
